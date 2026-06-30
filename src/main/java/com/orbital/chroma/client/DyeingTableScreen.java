ac7f595867ef248f0718e5a8cae3b80a8f3d1836
Orb4t3l <trevducket@gmail.com>
2026-06-30 16:18:28 -0700

    updated the screen and menu

--- src/main/java/com/orbital/chroma/client/DyeingTableScreen.java
+++ src/main/java/com/orbital/chroma/client/DyeingTableScreen.java
@@ -3,111 +3,147 @@ package com.orbital.chroma.client;
 import com.orbital.chroma.menu.DyeingTableMenu;
 import com.orbital.chroma.network.ChromaNetwork;
 import com.orbital.chroma.network.SetDyeColorPacket;
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractSliderButton;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+
+import java.util.function.IntConsumer;
 
 public class DyeingTableScreen extends AbstractContainerScreen<DyeingTableMenu> {
 
     private static final ResourceLocation TEXTURE = new ResourceLocation("chroma", "textures/gui/dyeing_table.png");
 
-    private ColorSlider redSlider;
-    private ColorSlider greenSlider;
-    private ColorSlider blueSlider;
+    private static final int SLIDER_X = 12;
+    private static final int SLIDER_W = 152;
+    private static final int SLIDER_H = 14;
+    private static final int SLIDER_R_Y = 48;
+    private static final int SLIDER_G_Y = 66;
+    private static final int SLIDER_B_Y = 84;
+
+    private static final int PREVIEW_X = 12;
+    private static final int PREVIEW_Y = 106;
+    private static final int PREVIEW_W = 76;
+    private static final int PREVIEW_H = 16;
+
+    private static final int HEX_X = 104;
+    private static final int HEX_Y = 106;
+    private static final int HEX_W = 60;
+    private static final int HEX_H = 16;
+
+    private GradientSlider redSlider;
+    private GradientSlider greenSlider;
+    private GradientSlider blueSlider;
     private EditBox hexBox;
 
     private int red;
     private int green;
     private int blue;
+    private boolean updatingFromHex = false;
+    private boolean updatingFromSlider = false;
 
     public DyeingTableScreen(DyeingTableMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
-        this.imageWidth = 196;
-        this.imageHeight = 202;
+        this.imageWidth = 176;
+        this.imageHeight = 204;
+        this.inventoryLabelY = this.imageHeight - 94;
     }
 
     @Override
     protected void init() {
         super.init();
-        int x = leftPos;
-        int y = topPos;
 
         int startColor = menu.getClientColor();
-        red = (startColor >> 16) & 0xFF;
-        green = (startColor >> 8) & 0xFF;
-        blue = startColor & 0xFF;
+        red   = (startColor >> 16) & 0xFF;
+        green = (startColor >> 8)  & 0xFF;
+        blue  =  startColor & 0xFF;
 
-        redSlider = new ColorSlider(x + 100, y + 20, "R", red, value -> {
-            red = value;
-            onColorChanged();
-        });
-        greenSlider = new ColorSlider(x + 100, y + 38, "G", green, value -> {
-            green = value;
-            onColorChanged();
-        });
-        blueSlider = new ColorSlider(x + 100, y + 56, "B", blue, value -> {
-            blue = value;
-            onColorChanged();
-        });
+        redSlider = new GradientSlider(
+                leftPos + SLIDER_X, topPos + SLIDER_R_Y, SLIDER_W, SLIDER_H,
+                red, Channel.RED,
+                v -> { if (!updatingFromHex) { red = v; onSliderChanged(); } }
+        );
+        greenSlider = new GradientSlider(
+                leftPos + SLIDER_X, topPos + SLIDER_G_Y, SLIDER_W, SLIDER_H,
+                green, Channel.GREEN,
+                v -> { if (!updatingFromHex) { green = v; onSliderChanged(); } }
+        );
+        blueSlider = new GradientSlider(
+                leftPos + SLIDER_X, topPos + SLIDER_B_Y, SLIDER_W, SLIDER_H,
+                blue, Channel.BLUE,
+                v -> { if (!updatingFromHex) { blue = v; onSliderChanged(); } }
+        );
 
         addRenderableWidget(redSlider);
         addRenderableWidget(greenSlider);
         addRenderableWidget(blueSlider);
 
-        hexBox = new EditBox(font, x + 100, y + 78, 70, 16, Component.literal("hex"));
+        hexBox = new EditBox(font, leftPos + HEX_X + 9, topPos + HEX_Y, HEX_W - 9, HEX_H, Component.empty());
         hexBox.setMaxLength(6);
+        hexBox.setBordered(true);
         hexBox.setResponder(this::onHexChanged);
         hexBox.setValue(String.format("%06X", startColor));
         addRenderableWidget(hexBox);
     }
 
-    private void onColorChanged() {
-        int color = (red << 16) | (green << 8) | blue;
-        hexBox.setValue(String.format("%06X", color));
-        sendColor(color);
+    private void onSliderChanged() {
+        updatingFromSlider = true;
+        hexBox.setValue(String.format("%06X", currentColor()));
+        updatingFromSlider = false;
+        sendColor();
     }
 
     private void onHexChanged(String value) {
-        if (value.length() != 6) {
-            return;
-        }
+        if (updatingFromSlider) return;
+        if (value.length() != 6) return;
         try {
             int color = Integer.parseInt(value, 16);
-            red = (color >> 16) & 0xFF;
-            green = (color >> 8) & 0xFF;
-            blue = color & 0xFF;
-            redSlider.setValueSilently(red);
-            greenSlider.setValueSilently(green);
-            blueSlider.setValueSilently(blue);
-            sendColor(color);
-        } catch (NumberFormatException ignored) {
-        }
+            updatingFromHex = true;
+            red   = (color >> 16) & 0xFF;
+            green = (color >> 8)  & 0xFF;
+            blue  =  color        & 0xFF;
+            redSlider.setChannel(red);
+            greenSlider.setChannel(green);
+            blueSlider.setChannel(blue);
+            updatingFromHex = false;
+            sendColor();
+        } catch (NumberFormatException ignored) {}
     }
 
-    private void sendColor(int color) {
-        ChromaNetwork.CHANNEL.sendToServer(new SetDyeColorPacket(color));
+    private int currentColor() {
+        return (red << 16) | (green << 8) | blue;
+    }
+
+    private void sendColor() {
+        ChromaNetwork.CHANNEL.sendToServer(new SetDyeColorPacket(currentColor()));
     }
 
     @Override
     protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
-        graphics.blit(TEXTURE, leftPos, topPos, 0, 0, imageWidth, imageHeight);
+        int x = leftPos;
+        int y = topPos;
 
-        int color = (red << 16) | (green << 8) | blue;
-        graphics.fill(leftPos + 100, topPos + 96, leftPos + 130, topPos + 116, 0xFF000000 | color);
-        graphics.renderOutline(leftPos + 99, topPos + 95, 32, 22, 0xFF1A1A1A);
+        graphics.blit(TEXTURE, x, y, 0, 0, imageWidth, imageHeight);
+
+        int color = currentColor();
+        graphics.fill(x + PREVIEW_X, y + PREVIEW_Y, x + PREVIEW_X + PREVIEW_W, y + PREVIEW_Y + PREVIEW_H, 0xFF000000 | color);
+        graphics.renderOutline(x + PREVIEW_X - 1, y + PREVIEW_Y - 1, PREVIEW_W + 2, PREVIEW_H + 2, 0xFF444444);
+
+        graphics.drawString(font, "#", x + HEX_X - 1, y + HEX_Y + 4, 0x555555, false);
     }
 
     @Override
     protected void renderLabels(GuiGraphics graphics, int mouseX, int mouseY) {
-        graphics.drawString(font, title, 8, 6, 0x3F3F3F, false);
-        graphics.drawString(font, Component.translatable("container.chroma.dyeing_table.color"), 100, 8, 0x3F3F3F, false);
+        graphics.drawString(font, title, 8, 6, 0x404040, false);
+        graphics.drawString(font, playerInventoryTitle, 8, inventoryLabelY, 0x404040, false);
+        graphics.drawString(font, "R", SLIDER_X - 1, SLIDER_R_Y + 3, 0xBB2222, false);
+        graphics.drawString(font, "G", SLIDER_X - 1, SLIDER_G_Y + 3, 0x22AA22, false);
+        graphics.drawString(font, "B", SLIDER_X - 1, SLIDER_B_Y + 3, 0x2222BB, false);
     }
 
     @Override
@@ -117,38 +153,68 @@ public class DyeingTableScreen extends AbstractContainerScreen<DyeingTableMenu> 
         renderTooltip(graphics, mouseX, mouseY);
     }
 
-    private static class ColorSlider extends AbstractSliderButton {
+    enum Channel { RED, GREEN, BLUE }
 
-        private final String label;
-        private final java.util.function.IntConsumer onChange;
-        private boolean silent = false;
+    private static final class GradientSlider extends AbstractSliderButton {
 
-        protected ColorSlider(int x, int y, String label, int initialValue, java.util.function.IntConsumer onChange) {
-            super(x, y, 70, 14, Component.literal(label + ": " + initialValue), initialValue / 255.0);
-            this.label = label;
+        private final Channel channel;
+        private final IntConsumer onChange;
+
+        GradientSlider(int x, int y, int width, int height, int initialValue, Channel channel, IntConsumer onChange) {
+            super(x, y, width, height, Component.literal(String.valueOf(initialValue)), initialValue / 255.0);
+            this.channel = channel;
             this.onChange = onChange;
         }
 
-        public void setValueSilently(int value) {
-            this.silent = true;
-            this.value = value / 255.0;
+        int getChannelValue() {
+            return (int) Math.round(value * 255);
+        }
+
+        void setChannel(int v) {
+            this.value = v / 255.0;
             updateMessage();
-            this.silent = false;
         }
 
         @Override
         protected void updateMessage() {
-            int intValue = (int) Math.round(value * 255);
-            setMessage(Component.literal(label + ": " + intValue));
+            setMessage(Component.literal(String.valueOf(getChannelValue())));
         }
 
         @Override
         protected void applyValue() {
-            if (silent) {
-                return;
-            }
-            int intValue = (int) Math.round(value * 255);
-            onChange.accept(intValue);
+            onChange.accept(getChannelValue());
+        }
+
+        @Override
+        public void renderWidget(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+            int x = getX();
+            int y = getY();
+            int w = getWidth();
+            int h = getHeight();
+
+            int darkColor = switch (channel) {
+                case RED   -> 0xFF1A0000;
+                case GREEN -> 0xFF001A00;
+                case BLUE  -> 0xFF00001A;
+            };
+            int fullColor = switch (channel) {
+                case RED   -> 0xFFFF0000;
+                case GREEN -> 0xFF00FF00;
+                case BLUE  -> 0xFF0000FF;
+            };
+
+            graphics.fillGradient(x, y, x + w, y + h, darkColor, fullColor);
+            graphics.renderOutline(x - 1, y - 1, w + 2, h + 2, isHovered ? 0xFFAAAAAA : 0xFF555555);
+
+            int handleX = x + (int) (value * (w - 6));
+            graphics.fill(handleX, y - 1, handleX + 6, y + h + 1, 0xFFFFFFFF);
+            graphics.fill(handleX + 1, y, handleX + 5, y + h, 0xFF888888);
+
+            Font font = Minecraft.getInstance().font;
+            int v = getChannelValue();
+            int labelX = x + w + 4;
+            int labelY = y + (h - 8) / 2;
+            graphics.drawString(font, String.valueOf(v), labelX, labelY, 0xFF404040, false);
         }
     }
 }
--- src/main/java/com/orbital/chroma/menu/DyeingTableMenu.java
+++ src/main/java/com/orbital/chroma/menu/DyeingTableMenu.java
@@ -7,29 +7,34 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.SimpleContainerData;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.SlotItemHandler;
 
 public class DyeingTableMenu extends AbstractContainerMenu {
 
+    public static final int SLOT_X = 80;
+    public static final int SLOT_Y = 20;
+
+    public static final int INV_ROW_Y = 122;
+    public static final int HOTBAR_Y = 180;
+
     public final DyeingTableBlockEntity blockEntity;
-    private final Level level;
     private final SimpleContainerData colorData = new SimpleContainerData(3);
 
     public DyeingTableMenu(int containerId, Inventory playerInventory, DyeingTableBlockEntity blockEntity) {
         super(ChromaMenus.DYEING_TABLE.get(), containerId);
         this.blockEntity = blockEntity;
-        this.level = playerInventory.player.level();
 
-        addSlot(new SlotItemHandler(blockEntity.getItemHandler(), 0, 80, 35));
+        addSlot(new SlotItemHandler(blockEntity.getItemHandler(), 0, SLOT_X, SLOT_Y));
 
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 9; col++) {
-                addSlot(new net.minecraft.world.inventory.Slot(playerInventory, col + row * 9 + 9, 8 + col * 18, 84 + row * 18));
+                addSlot(new Slot(playerInventory, col + row * 9 + 9, 8 + col * 18, INV_ROW_Y + row * 18));
             }
         }
         for (int col = 0; col < 9; col++) {
-            addSlot(new net.minecraft.world.inventory.Slot(playerInventory, col, 8 + col * 18, 142));
+            addSlot(new Slot(playerInventory, col, 8 + col * 18, HOTBAR_Y));
         }
 
         addDataSlots(colorData);
@@ -58,23 +63,23 @@ public class DyeingTableMenu extends AbstractContainerMenu {
     }
 
     @Override
-    public net.minecraft.world.item.ItemStack quickMoveStack(Player player, int index) {
-        net.minecraft.world.item.ItemStack result = net.minecraft.world.item.ItemStack.EMPTY;
-        net.minecraft.world.inventory.Slot slot = slots.get(index);
+    public ItemStack quickMoveStack(Player player, int index) {
+        ItemStack result = ItemStack.EMPTY;
+        Slot slot = slots.get(index);
         if (slot != null && slot.hasItem()) {
-            net.minecraft.world.item.ItemStack stackInSlot = slot.getItem();
+            ItemStack stackInSlot = slot.getItem();
             result = stackInSlot.copy();
             if (index == 0) {
                 if (!moveItemStackTo(stackInSlot, 1, 37, true)) {
-                    return net.minecraft.world.item.ItemStack.EMPTY;
+                    return ItemStack.EMPTY;
                 }
             } else {
                 if (!moveItemStackTo(stackInSlot, 0, 1, false)) {
-                    return net.minecraft.world.item.ItemStack.EMPTY;
+                    return ItemStack.EMPTY;
                 }
             }
             if (stackInSlot.isEmpty()) {
-                slot.set(net.minecraft.world.item.ItemStack.EMPTY);
+                slot.set(ItemStack.EMPTY);
             } else {
                 slot.setChanged();
             }
