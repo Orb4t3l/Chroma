17e2db8807baa44e99b23d0735d27d2c193f445e
Orb4t3l <trevducket@gmail.com>
2026-07-16 14:11:52 -0700

    fixed more stuff

--- src/main/java/com/orbital/chroma/item/ChromaBannerItem.java
+++ src/main/java/com/orbital/chroma/item/ChromaBannerItem.java
@@ -5,7 +5,6 @@ import net.minecraft.world.item.StandingAndWallBlockItem;
 import net.minecraft.world.level.block.Block;
 
 public class ChromaBannerItem extends StandingAndWallBlockItem {
-
     public ChromaBannerItem(Block floorBlock, Block wallBlock, Properties properties) {
         super(floorBlock, wallBlock, properties, Direction.DOWN);
     }
