0f4b9f34e0b2b01f53625e8136fa1bfe3bb16ea6
Orb4t3l <trevducket@gmail.com>
2026-07-23 16:52:31 -0700

    added the glass pane block, the jsons for it, and crafting recipes

---
+++ src/main/java/com/orbital/chroma/block/ChromaStainedGlassPaneBlock.java
@@ -0,0 +1,54 @@
+package com.orbital.chroma.block;
+
+import com.orbital.chroma.api.DyeableBlockEntity;
+import com.orbital.chroma.blockentity.ChromaDyeableBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.IronBarsBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+import javax.annotation.Nullable;
+import java.util.function.Supplier;
+
+public class ChromaStainedGlassPaneBlock extends IronBarsBlock implements EntityBlock {
+
+    private final Supplier<BlockEntityType<ChromaDyeableBlockEntity>> beType;
+
+    public ChromaStainedGlassPaneBlock(Properties properties, Supplier<BlockEntityType<ChromaDyeableBlockEntity>> beType) {
+        super(properties);
+        this.beType = beType;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new ChromaDyeableBlockEntity(beType.get(), pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(
+            Level level, BlockState state, BlockEntityType<T> type) {
+        return null;
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos pos, BlockState state,
+                            @Nullable LivingEntity placer, ItemStack stack) {
+        super.setPlacedBy(level, pos, state, placer, stack);
+        BlockEntity be = level.getBlockEntity(pos);
+        if (be != null) DyeableBlockEntity.applyColorFromStack(be, stack);
+    }
+}
